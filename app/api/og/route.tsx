4cd75d80fe11839cbfe7a7950ec7926178128091
custompro98 <mitchjoa@gmail.com>
2024-02-12 13:55:08 -0500

    fix: Remove try/catch

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -14,71 +14,64 @@ enum Keyword {
 }
 
 export async function GET(request: Request) {
-  try {
-    const { searchParams } = new URL(request.url);
-    const idParam = z.coerce
-      .number()
-      .safeParse(getSearchParam(searchParams, Keyword.Id));
+  const { searchParams } = new URL(request.url);
+  const idParam = z.coerce
+    .number()
+    .safeParse(getSearchParam(searchParams, Keyword.Id));
 
-    if (!idParam.success) {
-      return notFound();
-    }
-
-    const [ogImageRecord] = await db
-      .select()
-      .from(ogImages)
-      .innerJoin(ogImageBlogs, eq(ogImages.id, ogImageBlogs.ogImageId))
-      .where(and(eq(ogImages.id, idParam.data), isNull(ogImages.deletedAt)))
-      .limit(1)
-      .execute();
-
-    if (!isPresent(ogImageRecord)) {
-      return notFound();
-    }
-
-    const title = ogImageRecord.og_image_blog.title;
-    const avatar = ogImageRecord.og_image_blog.icon;
-
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            fontSize: 40,
-            color: "black",
-            background: "white",
-            width: "100%",
-            height: "100%",
-            padding: "50px 200px",
-            textAlign: "center",
-            justifyContent: "center",
-            alignItems: "center",
-            display: "flex",
-          }}
-        >
-          <img
-            alt="brand logo"
-            src={
-              avatar
-                ? decodeURIComponent(avatar)
-                : `${process.env.NEXT_PUBLIC_HOSTNAME}/argo.png`
-            }
-            width="64px"
-            height="64px"
-          />
-          {title}
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-      },
-    );
-  } catch (e: any) {
-    console.log(`${e.message}`);
-    return new Response(`Failed to generate the image`, {
-      status: 500,
-    });
+  if (!idParam.success) {
+    return notFound();
   }
+
+  const [ogImageRecord] = await db
+    .select()
+    .from(ogImages)
+    .innerJoin(ogImageBlogs, eq(ogImages.id, ogImageBlogs.ogImageId))
+    .where(and(eq(ogImages.id, idParam.data), isNull(ogImages.deletedAt)))
+    .limit(1)
+    .execute();
+
+  if (!isPresent(ogImageRecord)) {
+    return notFound();
+  }
+
+  const title = ogImageRecord.og_image_blog.title;
+  const avatar = ogImageRecord.og_image_blog.icon;
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: "black",
+          background: "white",
+          width: "100%",
+          height: "100%",
+          padding: "50px 200px",
+          textAlign: "center",
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+        }}
+      >
+        <img
+          alt="brand logo"
+          src={
+            avatar
+              ? decodeURIComponent(avatar)
+              : `${process.env.NEXT_PUBLIC_HOSTNAME}/argo.png`
+          }
+          width="64px"
+          height="64px"
+        />
+        {title}
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
 }
 
 function getSearchParam(
